8a6ddadd8d07ac19ee36d3fd739bb7d2bb903dcd
Ezekiel <122766315+limiacJosh@users.noreply.github.com>
2023-01-23 11:47:55 +0800

    Add files via upload

---
+++ prelims/app.js
@@ -0,0 +1,2 @@
+var myLogModule = require('./utility/log.js');
+myLogModule.warning('Warning node not found..');
---
+++ prelims/node-example.js
@@ -0,0 +1,18 @@
+//LIMIAC, JOSHUA EZEKIEL T.
+//WD -201
+//Jan 16, 2023
+//Week 2 and 3 basics of Node.js
+
+
+
+
+
+
+console.log("welcome to WEBSERVER");
+
+function Display(x) {
+    console.log(x);
+
+}
+
+Display(200);
