c440b55ce2c93499d08cc5a44b035fc7a0c6b0bc
limiacJosh <122766315+limiacJosh@users.noreply.github.com>
2023-01-30 21:43:26 +0800

    2 files added

---
+++ prelims/Prelim Hands On Quiz/app.js
@@ -0,0 +1,33 @@
+const calc = require('./calculation.js');
+
+console.log('addition 2 + 2 = ', calc.add(2, 2));
+console.log('subtract 5 - 2 = ', calc.subtract(5, 2));
+console.log('multiply 3 2 = ', calc.multiply(3, 2));
+console.log('divide 10 / 2 = ', calc.divide(10, 2));
+console.log();
+
+const Rate = 300;
+const hoursWork = 4;
+const daysPerWeek = 6;
+const grossIncome = Rate * hoursWork * daysPerWeek;
+
+console.log('Rate per hour', Rate);
+console.log('Hours Worked', hoursWork);
+console.log('Days per week', daysPerWeek);
+console.log('Gross Income', grossIncome);
+console.log();
+
+const tax = calc.multiply(grossIncome, 0.1);
+const sss = 1200;
+const pagibig = 300; //24 total of work hours
+const philhealth = 400;
+const deductions = tax + sss + pagibig + philhealth;
+const netSalary = calc.subtract(grossIncome, deductions);
+
+console.log('Tax: ', tax);
+console.log('SSS:', sss);
+console.log('PAGIBIG:', pagibig);
+console.log('PhilHealth: ', philhealth);
+console.log('Deductions: ', deductions);
+console.log();
+console.log('Net Salary: ', netSalary);
