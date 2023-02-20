041dd3e303964d8cfd47f09241fa398e7cd8e4b6
limiacJosh <122766315+limiacJosh@users.noreply.github.com>
2023-02-20 12:09:52 +0800

    1 binaries uploaded

--- midterms/mid-serve-static/index.js
+++ midterms/mid-serve-static/index.js
@@ -3,12 +3,12 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/' + 'index.html');
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/' + 'index.html');
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
