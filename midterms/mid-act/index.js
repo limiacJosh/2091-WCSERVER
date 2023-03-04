795da444f4c59a8b90756164fd9c42e398911f55
limiacJosh <122766315+limiacJosh@users.noreply.github.com>
2023-03-04 22:22:38 +0800

    2 files modified, 1 added

--- midterms/mid-act/index.js
+++ midterms/mid-act/index.js
@@ -37,19 +37,22 @@ app.get('/', (req, res) => {
 });
 
 //get form
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/' + 'contact.html');
+
+app.use(express.urlencoded());
+
+app.get('/', (req, res, next) => {
+  res.send(`<form method="POST" action="/">
+  <input type="text" name="name" placeholder="name">
+  <input type="text" name="subject" placeholder="subject">
+  <input type="text" name="email" placeholder="email" >
+  <input type="text" name="message" placeholder="message" >
+
+  <input type="submit">
+</form>`);
 });
 
-app.get('/process_get', function (req, res) {
-  // Prepare output in JSON format
-  response = {
-    first_name: req.query.first_name,
-    last_name: req.query.last_name,
-  };
-
-  console.log(response);
-  res.end(JSON.stringify(response));
+app.post('/', function (req, res, next) {
+  res.send(JSON.stringify(req.body));
 });
 
 //app listen server
