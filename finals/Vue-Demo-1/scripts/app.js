776a2e7a4dc7654fef74b74a3add3ee21fe231b8
limiacJosh <122766315+limiacJosh@users.noreply.github.com>
2023-03-27 11:11:10 +0800

    4 files added

---
+++ finals/Vue-Demo-1/scripts/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title: 'The Ledger',
+            author: 'Joe Esquivel',
+            age: 34
+        }
+    }
+
+});
+
+app.mount('#app')
